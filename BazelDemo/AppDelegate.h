604617d1ec1be87439a37fcb63ecbe9d7229cee2
liushuhao.axe <liushuhao.axe@bytedance.com>
2022-06-16 14:59:41 +0800

    Initial Commit

---
+++ BazelDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  BazelDemo
+//
+//  Created by Sharker on 6/16/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ BazelDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  BazelDemo
+//
+//  Created by Sharker on 6/16/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
